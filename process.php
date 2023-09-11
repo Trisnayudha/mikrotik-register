ea419822ee96baa2a7ef387aa1542b467d868b17
Yudha <48747021+Trisnayudha@users.noreply.github.com>
2023-09-11 20:35:03 +0700

    update

--- process.php
+++ process.php
@@ -7,11 +7,10 @@ $password_mikrotik = "";
 $ip_mikrotik = "192.168.2.1";
 
 // Get database connection parameters from environment variables
-$db_host = getenv('DB_HOST');
-$db_username = getenv('DB_USERNAME');
-$db_password = getenv('DB_PASSWORD');
-$db_name = getenv('DB_NAME');
-
+$db_host = '127.0.0.1';
+$db_username = 'root';
+$db_password = '';
+$db_name = 'dmc';
 if ($db_host === false || $db_username === false || $db_password === false || $db_name === false) {
     die("Database configuration missing or invalid.");
 }
@@ -25,15 +24,17 @@ if ($mysqli->connect_error) {
 
 if ($API->connect($ip_mikrotik, $user_mikrotik, $password_mikrotik)) {
     $username = $mysqli->real_escape_string($_POST['username']);
-    $password = $mysqli->real_escape_string($_POST['password']);
     $name = $mysqli->real_escape_string($_POST['name']);
     $phone = $mysqli->real_escape_string($_POST['phone']);
     $company = $mysqli->real_escape_string($_POST['company']);
     $job = $mysqli->real_escape_string($_POST['job']);
+    $password = $mysqli->real_escape_string($_POST['password']);
+    $member = $mysqli->real_escape_string($_POST['member']);
+    $newsletter = $mysqli->real_escape_string($_POST['newsletter']);
 
     // Insert registration data into the database
-    $insert_query = "INSERT INTO user_registration (username, password, name, phone, company, job) 
-                     VALUES ('$username', '$password', '$name', '$phone', '$company', '$job')";
+    $insert_query = "INSERT INTO mikrotik_users (username, name, phone, company, job, cci,explore) 
+                     VALUES ('$username', '$name', '$phone', '$company', '$job','$member','$newsletter')";
     
     if ($mysqli->query($insert_query)) {
         // Registration data inserted successfully
